d12256f43fe4d94f73417c38cc114ef79cb6e304
achdj <achilledjegnon@gmail.com>
2022-11-09 20:47:19 +0000

    add app usd for other file

--- myApp.js
+++ myApp.js
@@ -2,12 +2,20 @@ let express = require('express');
 let app = express();
 console.log('Hello World');
 
+
+//path = __dirname + "/public";
+
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
 
     absolutePath = __dirname + "/views/index.html";
 
     res.sendFile(absolutePath);
 });
+
+
+
 
 
 
